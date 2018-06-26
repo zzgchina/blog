1b9ef854d9a06ba877817af17a002d9165186be6
zhuzhengguo <1542761996@qq.com>
2018-06-26 18:11:39 +0800

    更新

--- blog.sql
+++ blog.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: 2018-06-21 10:01:36
+-- Generation Time: 2018-06-26 10:09:08
 -- 服务器版本： 5.7.21
 -- PHP Version: 7.0.29
 
@@ -25,19 +25,48 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- 表的结构 `blog_article`
+--
+
+DROP TABLE IF EXISTS `blog_article`;
+CREATE TABLE IF NOT EXISTS `blog_article` (
+  `id` int(11) NOT NULL COMMENT '文章',
+  `name` char(15) NOT NULL COMMENT '名称',
+  `content` text NOT NULL COMMENT '内容',
+  `author` char(15) NOT NULL COMMENT '作者',
+  `way` tinyint(4) NOT NULL DEFAULT '1' COMMENT '文章来源：1;原创，2：装载',
+  `url` varchar(100) NOT NULL COMMENT '装载来源地址',
+  `adddate` int(11) NOT NULL COMMENT '添加时间',
+  `hot` int(11) NOT NULL DEFAULT '0' COMMENT '点击率',
+  `private` tinyint(4) NOT NULL DEFAULT '1' COMMENT '文章属性：1：公开，2：私密',
+  `sort` int(11) NOT NULL COMMENT '栏目分类ID',
+  `talk` tinyint(4) NOT NULL DEFAULT '1' COMMENT '1:可评论，2，不可评论'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- 表的结构 `blog_column`
 --
 
 DROP TABLE IF EXISTS `blog_column`;
 CREATE TABLE IF NOT EXISTS `blog_column` (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '栏目表',
-  `name` int(11) NOT NULL COMMENT '名称',
-  `status` tinyint(1) NOT NULL COMMENT '状态，1允许',
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '分类表',
+  `name` varchar(15) NOT NULL COMMENT '名称',
+  `status` tinyint(1) NOT NULL DEFAULT '1' COMMENT '状态，1允许',
   `adddate` int(11) NOT NULL COMMENT '添加日期',
-  `pid` int(11) NOT NULL DEFAULT '1' COMMENT '上级栏目id,顶级栏目为1',
-  `dexscript` int(11) NOT NULL COMMENT '栏目描述',
+  `descript` text NOT NULL COMMENT '分类描述',
+  `token` char(32) NOT NULL COMMENT '插入验证',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+--
+-- 转存表中的数据 `blog_column`
+--
+
+INSERT INTO `blog_column` (`id`, `name`, `status`, `adddate`, `descript`, `token`) VALUES
+(1, 'php', 1, 1529999787, 'df', '9e9d7f3d6e63930ae57264313ddf1405'),
+(2, '前端', 1, 1530000034, '前端内容', '3317763459f6bbea52af518bf809976e');
 
 -- --------------------------------------------------------
 
@@ -56,17 +85,19 @@ CREATE TABLE IF NOT EXISTS `blog_menu` (
   `url` varchar(20) NOT NULL COMMENT '菜单路由',
   `token` varchar(32) NOT NULL COMMENT '修改检验值',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
 
 --
 -- 转存表中的数据 `blog_menu`
 --
 
 INSERT INTO `blog_menu` (`id`, `name`, `ico`, `pid`, `addtime`, `status`, `url`, `token`) VALUES
-(4, 'gd', 'ff', 2, 1529402615, 1, 'fdd', '1'),
-(5, '栏目', 'ambulance', 0, 1529575045, 1, '1', 'abea9603b0b33b91d2bd0f93f552915a'),
-(6, '栏目1', 'e', 5, 1529575228, 1, 'q', '0d2f43026d263c86499be4c346c4e541'),
-(7, '栏目3', 'd', 5, 1529575253, 1, 'd', '5480060aa2fa7e62c76f6f411e8afeda');
+(5, '分类管理', 'th-list', 0, 1529983041, 1, '1', '2abdf34ab11a6e3fbc7dc27021e79570'),
+(6, '分类列表', 'hand-o-right', 5, 1529983952, 1, 'column/index', '69d3522e9cf9cac20ad703420fd4df6e'),
+(7, '文章管理', 'file', 0, 1530000123, 1, '1', 'a3bd8adc4ca4a568732f924e59112943'),
+(10, '文章列表', 'file-text', 7, 1530000192, 1, 'article/index', '624ba0d9f1108d42902e05925a736183'),
+(11, 'sf vx', 'ambulance', 5, 1529920192, 1, 'sfs', 'ccbd1b57959cffc3e224375521c1f9b4'),
+(12, 'sf', 'ambulance', 10, 1529920202, 1, 'sf', '04b2e81944143ed5a60dead5fc30402c');
 
 -- --------------------------------------------------------
 
